3c64a7474f0be45b98ea37914aaa4129ab7cf372
S1LVACK <safinig2010@gmail.com>
2024-01-16 16:11:27 +0500

    first commit

---
+++ problem3.js
@@ -0,0 +1,16 @@
+// let InputNumber = 5;
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(InputNumber ,'*', i, '=', InputNumber * i);
+    
+// }
+
+// let Number = 939;
+// Number %=10;
+
+// if(Number == 9){
+//     console.log('YES');
+// }
+// else{
+//     console.log('NO');
+// }
---
+++ tasks.js
@@ -0,0 +1,44 @@
+// task 2
+// let Number = -9;
+
+// if(Number > 0){
+//     console.log('Positive');
+// }
+// else if(Number < 0){
+//     console.log('Negative');
+// }
+// else if(Number == 0){
+//     console.log('Zero');
+// }
+
+
+// task 1
+
+// let FirstNumber = 2;
+// let SecondNumber = 5;
+
+// console.log(FirstNumber + SecondNumber);
+
+
+// task 3
+
+// let InputNumber = 5;
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(InputNumber ,'*', i, '=', InputNumber * i);
+    
+// }
+
+// task 4
+
+
+// let Number = 939;
+// Number %=10;
+
+// if(Number == 9){
+//     console.log('YES');
+// }
+// else{
+//     console.log('NO');
+// }
+
